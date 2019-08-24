68c82b6a8fd48d768889bfbca1063be363d90b16
d3vsupp0r7 <dev@dev.com>
2019-08-24 13:23:49 +0200

    Add ResourceBundle

--- springboot2-employee-mvc/src/main/java/org/lba/springboot2/Springboot2AppEmployeeMvc.java
+++ springboot2-employee-mvc/src/main/java/org/lba/springboot2/Springboot2AppEmployeeMvc.java
@@ -3,7 +3,10 @@ package org.lba.springboot2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -14,6 +17,14 @@ public class Springboot2AppEmployeeMvc {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2AppEmployeeMvc.class, args);
+	}
+	
+	@Bean
+	@Description("Spring Message Resolver")
+	public ResourceBundleMessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages");
+	    return messageSource;
 	}
 
 }
